482ad9c9123013792870ed41b9e11f42d4b24a06
Mano K <126533657+manov-ik@users.noreply.github.com>
2024-02-05 12:47:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 function updateIframe() {
-    
     var searchInput = document.getElementById('searchInput').value;
 
-    var split_index = searchInput.indexOf("=");
-    
-    var vid_embed_code = searchInput.slice(split_index+1 , split_index+12);
+    if (searchInput !== "") {
+        var split_index = searchInput.indexOf("=");
 
-    var vid="https://www.youtube.com/embed/"+vid_embed_code;
+        if (split_index !== -1) {
+            var vid_embed_code = searchInput.slice(split_index + 1, split_index + 12);
 
-    console.log(vid);
-    videoIframe.src = vid;
+            var vid = "https://www.youtube.com/embed/" + vid_embed_code;
+
+            console.log(vid);
+            videoIframe.src = vid;
+        } else {
+            alert("Invalid YouTube video URL");
+        }
+    } else {
+        alert("Enter a YouTube video URL");
+    }
 }
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        updateIframe();
+    }
+}
+
+var searchInput = document.getElementById('searchInput');
+searchInput.addEventListener("keypress", handleKeyPress);
